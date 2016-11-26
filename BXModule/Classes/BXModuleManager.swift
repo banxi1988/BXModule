6f2e695bfe0f1015fe473fa9d75f337292084798
banxi1988 <banxi1988@gmail.com>
2016-11-26 21:56:15 +0800

    fix some warnings

--- BXModule/Classes/BXModuleManager.swift
+++ BXModule/Classes/BXModuleManager.swift
@@ -29,10 +29,21 @@ open class BXModuleManager: NSObject{
 }
 
 extension BXModuleManager: UIApplicationDelegate{
+  
+  // MARK: normal lifecycle
+  
+  public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    NSLog("\(#function) \(application)  \(launchOptions)")
+    for module in all(){
+      let _ = module.application?(application, willFinishLaunchingWithOptions: launchOptions)
+    }
+    return true
+  }
+  
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
     NSLog("\(#function) \(application)  \(launchOptions)")
     for module in all(){
-      module.application?(application, didFinishLaunchingWithOptions: launchOptions)
+      let _ = module.application?(application, didFinishLaunchingWithOptions: launchOptions)
     }
     return true
   }
@@ -66,9 +77,63 @@ extension BXModuleManager: UIApplicationDelegate{
   }
   
   public func applicationWillTerminate(_ application: UIApplication) {
-    NSLog("\(application) willTerminate")
+    NSLog("\(#function) \(application)")
     for module in all(){
       module.applicationWillTerminate?(application)
     }
   }
+  
+  // MARK:  Remote Notification
+ 
+  // 已经实现了下面的 带fetchCompletionHandler 的方法,此方法就不会再调用
+//  public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+//    for module in all(){
+//      module.application?(application, didReceiveRemoteNotification: userInfo)
+//    }
+//  }
+//  
+  public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    for module in all(){
+        module.application?(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+    }
+  }
+  
+  public func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
+    NSLog("\(#function) \(application)")
+    for module in all(){
+      module.application?(application, didRegister: notificationSettings)
+    }
+  }
+ 
+  public  func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable: Any], completionHandler: @escaping () -> Void) {
+    NSLog("\(#function) \(application)")
+    for module in all(){
+      module.application?(application, handleActionWithIdentifier: identifier, forRemoteNotification: userInfo, completionHandler: completionHandler)
+    }
+  }
+  
+  public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    NSLog("\(#function) \(application)")
+    for module in all(){
+      module.application?(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+  }
+  
+  public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    NSLog("\(#function) \(application)")
+    for module in all(){
+      module.application?(application, didFailToRegisterForRemoteNotificationsWithError: error)
+    }
+  }
+  
+  // MARK: Local Notification
+  
+  public func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+    
+    NSLog("\(#function) \(application)")
+    for module in all(){
+      module.application?(application, didReceive: notification)
+    }
+  }
+  
 }
